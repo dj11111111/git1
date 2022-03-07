7181718e5a392f49d793665d4dab84f8da075499
dj <2556560122@qq.com>
2022-03-07 10:04:15 +0800

    first commit

---
+++ src/main/java/com/itguigu/git/Git.java
@@ -0,0 +1,9 @@
+package com.itguigu.git;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+    }
+}
